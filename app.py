04c83134073d3743d9dbb98bc4843d5c508e37de
vineet kukreti <91364632+vineetkukreti@users.noreply.github.com>
2024-01-15 18:50:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(
     page_title="VineetAI",
     page_icon="🌌",
     layout="wide",
-      bg_color="#FF0000"
+      bg_color="#FF0000",
 )
 
 # Custom styling for background color
